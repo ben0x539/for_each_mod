d06da9aed106928dbea99ab58e8a16bb0afcb7f5
Benjamin Herr <ben@0x539.de>
2021-10-27 03:18:39 -0700

    hardcode modules dir

--- examples/foo.rs
+++ examples/foo.rs
@@ -2,6 +2,7 @@ extern crate for_each_mod;
 use for_each_mod::*;
 
 for_each_mod! {
+    #[path = "modules/PLACEHOLDER.rs"]
     mod PLACEHOLDER;
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -21,22 +21,22 @@ pub fn for_each_mod(input: TokenStream) -> TokenStream {
 fn for_each_mod_inner(input: TokenStream) -> Result<TokenStream> {
     let caller_path = Span::call_site().source_file().path();
     let caller_dir = caller_path.parent().ok_or("no dir??")?;
+    let modules_dir = caller_dir.join("modules");
 
     let input_str = input.to_string();
     let mut output = String::new();
     
-    for entry in fs::read_dir(caller_dir)? {
+    for entry in fs::read_dir(modules_dir)? {
         let entry = entry?;
         let path = entry.path();
 
-        if path == caller_path { continue; }
         if path.extension() != Some(&OsStr::new("rs")) { continue; }
         let file_name = path.file_stem().unwrap().to_str().unwrap();
 
         output += &input_str.replace("PLACEHOLDER", file_name);
     }
 
-    eprintln!("output is: {}", output);
+    //eprintln!("output is: {}", output);
 
     Ok(output.parse()?)
 }
